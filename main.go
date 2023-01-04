8a73cd13f97e855147bf3b15b58e1895546dbc7a
AyrtonMoises <ayrton.sousa@gmail.com>
2023-01-04 10:36:30 -0300

    alteração teste

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "<h1>I am Web App in Go</h1>")
+	fmt.Fprintf(w, "<h1>I am Web App in Go in 2023</h1>")
 }
 
 func main(){
